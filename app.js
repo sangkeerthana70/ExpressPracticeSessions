fa2e0502766363ebddc6623856e503a6aa36e971
Anuradha <sangkeerthana@gmail.com>
2018-01-26 21:27:14 +0000

    met additional requirements of Level3 in ExpressCodeSchool

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 /*when user clicks the homepage it should show two buttons
 First button suggests good food.
-Second five good restaurants.*/
+Second button suggets five good restaurants.*/
 
-
+/* the following code is an example of Level2 in express Code School
 var express = require("express");//require the express library
 var app = express();//application instance
 
-app.use(express.static('public')); 
+app.use(express.static('public')); //default route which is the middleware
 
 app.get('/suggestionForGoodFood', function(req, res){
   var food = ['Poori and channa', 'Chicken Biryani', 'Masal dosa', 'Pani poori'];
@@ -17,6 +17,37 @@ app.get('/suggestionForGoodRestaurants', function(req, res) {
     var restaurants = ['Godavari', 'Narmadha', 'Curry house', 'Minerva'];
     res.send(restaurants);
 })
+
+app.listen(process.env.PORT, function() {
+    console.log("listening on Port.....");
+});*/
+
+var express = require("express");//require the express library
+var app = express();//application instance
+
+app.use(express.static('public')); //default route which is the middleware
+
+app.get('/suggestionForGoodFood', function(request, response){
+  var food = ['Pizza', 'Chicken Tikka masala', 'Cheese quasadilla', 'Cheesecake'];
+      if (request.query.limit >= 0) {
+        response.json(food.slice(0, request.query.limit));
+    }
+    else {
+        response.json(food);
+    }
+ 
+});
+
+app.get('/suggestionForGoodRestaurants', function(request, response) {
+    var restaurants = ['Olive Garden', 'Godavari', 'IHOP', 'Acapulcas'];
+    if (request.query.cnt >= 0) {
+        response.json(restaurants.slice(0, request.query.cnt));
+    }
+    else {
+        response.json(restaurants);
+    }
+    
+});
 
 app.listen(process.env.PORT, function() {
     console.log("listening on Port.....");
--- public/client.js
+++ public/client.js
@@ -1,20 +1,29 @@
 /*global $ */
 
 $('#food').click(function(event) {
+    var foodCount = $('#foodCount').val();
+    //console.log(foodCount);
      event.preventDefault();
-   $.get('/suggestionForGoodFood', function(food) {//using route to make the AJAX call.
+   $.get('/suggestionForGoodFood?limit=' + foodCount, function(food) {//using route to make 
+   //the AJAX call and also passing the parameter value for the limit query.
+   
        $("#goodFood").empty();
+       
        for (var i=0;i<food.length;i++) {
            var list = document.createElement('Li');//create a List item
            list.innerHTML = food[i];//populate the Li
 			$('#goodFood').append(list);//append for each run
+			
        }
      });  
 });
 
 $('#restaurant').click(function(event) {
      event.preventDefault();
-     $.get('/suggestionForGoodRestaurants', function(restaurants) {
+     
+     var restaurantCount = $('#restaurantCount').val();
+     console.log(restaurantCount);
+     $.get('/suggestionForGoodRestaurants?cnt=' + restaurantCount, function(restaurants) {
         $("#goodRestaurant").empty();
         for (var i=0; i<restaurants.length; i++) {
             var list = document.createElement('Li');
